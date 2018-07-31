5126486f78e062bd524fe7d50bd4a38ecedd7b85
thejosan <33949813+thejosan@users.noreply.github.com>
2018-07-31 20:53:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i "s/yourlogstashurl/$LOGSTASH_URL/g" /filebeat.yml 
-sed -i "s/yourlog4dir/$LOG4_DIR/g" /filebeat.yml 
-sed -i "s/yournginxdir/$NGINX_DIR/g" /filebeat.yml 
-sed -i "s/yourpm2dir/$PM2_DIR/g" /filebeat.yml 
+sed -i "s|yourlogstashurl|$LOGSTASH_URL|g" /filebeat.yml 
+sed -i "s|yourlog4dir|$LOG4_DIR|g" /filebeat.yml 
+sed -i "s|yournginxdir|$NGINX_DIR|g" /filebeat.yml 
+sed -i "s|yourpm2dir|$PM2_DIR|g" /filebeat.yml 
 /filebeat -e -c /filebeat.yml -d "publish"
